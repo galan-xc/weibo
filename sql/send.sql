3d36c4377fde5d894ca5e053c00212beeb931cc8
galan-xc <galanwandering@gmail.com>
2021-05-02 21:46:37 +0800

    ...

--- sql/send.sql
+++ sql/send.sql
@@ -2,7 +2,7 @@ CREATE TABLE `send` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `uid` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
   `msg` varchar(800) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `ret` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `ret` varchar(300) COLLATE utf8mb4_unicode_ci NOT NULL,
   `create_time` datetime NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=602 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
